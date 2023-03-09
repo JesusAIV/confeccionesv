264e92d74507f96f0ac4a6e091e955a020988803
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-03-09 10:46:33 -0500

    Aplicate responsive section categorías v1

--- view/content/inicio.php
+++ view/content/inicio.php
@@ -1,3 +1,4 @@
+<!-- Inicio Slider -->
 <div class="slider">
     <div class="slider-wrapper">
         <div class="slider-slide">
@@ -11,3 +12,66 @@
     <div class="slider-button-prev"></div>
     <div class="slider-button-next"></div>
 </div>
+<!-- Fin SLider -->
+
+<!-- Inicio Seccion Categorias -->
+<div class="section-categorias">
+    <div class="categorias-titulo">
+        <h2>Categorías</h2>
+    </div>
+    <div class="categorías-contenido">
+        <div class="content-categorias">
+            <div class="content-categoria-img">
+                <img class="categoria-img" src="<?php echo SERVERURL.'view/assets/img/categoria/bolso.jpg-1669676686.jpeg' ?>" alt="">
+            </div>
+            <div class="content-cat-detalles">
+                <div class="content-cat-titulo">
+                    <p class="content-cat-p">Carteras</p>
+                </div>
+                <div class="content-cat-link">
+                    <a href="#" class="content-cat-a">Ver más</a>
+                </div>
+            </div>
+        </div>
+        <div class="content-categorias">
+            <div class="content-categoria-img">
+                <img class="categoria-img" src="<?php echo SERVERURL.'view/assets/img/categoria/carteras.jpg-1669256184.jpeg' ?>" alt="">
+            </div>
+            <div class="content-cat-detalles">
+                <div class="content-cat-titulo">
+                    <p class="content-cat-p">Carteras</p>
+                </div>
+                <div class="content-cat-link">
+                    <a href="#" class="content-cat-a">Ver más</a>
+                </div>
+            </div>
+        </div>
+        <div class="content-categorias">
+            <div class="content-categoria-img">
+                <img class="categoria-img" src="<?php echo SERVERURL.'view/assets/img/categoria/mochilas.jpg-1669256282.jpeg' ?>" alt="">
+            </div>
+            <div class="content-cat-detalles">
+                <div class="content-cat-titulo">
+                    <p class="content-cat-p">Carteras</p>
+                </div>
+                <div class="content-cat-link">
+                    <a href="#" class="content-cat-a">Ver más</a>
+                </div>
+            </div>
+        </div>
+        <div class="content-categorias">
+            <div class="content-categoria-img">
+                <img class="categoria-img" src="<?php echo SERVERURL.'view/assets/img/categoria/morrales.jpg-1669256251.jpeg' ?>" alt="">
+            </div>
+            <div class="content-cat-detalles">
+                <div class="content-cat-titulo">
+                    <p class="content-cat-p">Carteras</p>
+                </div>
+                <div class="content-cat-link">
+                    <a href="#" class="content-cat-a">Ver más</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Fin Seccion Categorias -->
